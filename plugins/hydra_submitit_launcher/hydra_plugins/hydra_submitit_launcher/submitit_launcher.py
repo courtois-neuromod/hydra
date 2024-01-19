9e227a095345b5ea3d914efa95b74b622e7d1fdf
Maximilien Le Cleï <34255811+MaximilienLC@users.noreply.github.com>
2024-01-19 17:57:50 -0500

    making local submit one job at a time

--- plugins/hydra_submitit_launcher/hydra_plugins/hydra_submitit_launcher/submitit_launcher.py
+++ plugins/hydra_submitit_launcher/hydra_plugins/hydra_submitit_launcher/submitit_launcher.py
@@ -143,9 +143,16 @@ class BaseSubmititLauncher(Launcher):
                     Singleton.get_state(),
                 )
             )
-
-        jobs = executor.map_array(self, *zip(*job_params))
-        return [j.results()[0] for j in jobs]
+        
+        if self._EXECUTOR == "local":
+            results = []
+            for i, job_param in enumerate(job_params):
+                job = executor.map_array(self, *zip(*[job_params[i]]))
+                results.append(job[0].results()[0])
+            return results
+        else: # self._EXECUTOR == "false":
+            jobs = executor.map_array(self, *zip(*job_params))
+            return [j.results()[0] for j in jobs]
 
 
 class LocalLauncher(BaseSubmititLauncher):
